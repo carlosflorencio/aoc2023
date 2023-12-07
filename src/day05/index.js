5e0befdf53a6b8d8fabad23b4f06efb1736bef04
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-12-07 00:08:02 +0000

    day 5 part 2

--- src/day05/index.js
+++ src/day05/index.js
@@ -34,37 +34,72 @@ const parseInput = (rawInput) => {
 const part1 = (rawInput) => {
   const input = parseInput(rawInput)
 
-  const locations = []
+  let location = Number.MAX_SAFE_INTEGER
   for (const seed of input.seeds) {
-    let loc = seed
-    for (let map of input.maps) {
-      for (const line of map) {
-        const [dest, src, range] = line
-
-        if (src <= loc && loc < src + range) {
-          loc = dest + (loc - src)
-          break
-        }
-      }
+    const loc = getSeedLocation(seed, input.maps)
+    if (loc < location) {
+      location = loc
     }
-
-    locations.push(loc)
   }
 
-  return Math.min(...locations)
+  return location
 }
 
+// brute force
 const part2 = (rawInput) => {
   const input = parseInput(rawInput)
 
-  return
+  let first = input.maps[0].map((x) => ({
+    start: x[1],
+    end: x[1] + x[2],
+  }))
+
+  let location = Number.MAX_SAFE_INTEGER
+  for (let i = 0; i < input.seeds.length; i += 2) {
+    let start = input.seeds[i]
+    let end = start + input.seeds[i + 1] - 1
+    let loc = Number.MAX_SAFE_INTEGER
+
+    for (let j = 0; j < first.length; j++) {
+      let overlap =
+        Math.min(end, first[j].end) - Math.max(start, first[j].start)
+
+      if (overlap >= 0) {
+        let overLapStart = start >= first[j].start ? start : first[j].start
+        let overLapEnd = end <= first[j].end ? end : first[j].end
+
+        for (let k = overLapStart; k <= overLapEnd; k++) {
+          loc = getSeedLocation(k, input.maps)
+          if (loc < location) {
+            location = loc
+          }
+        }
+
+        break
+      }
+    }
+  }
+
+  return location
 }
 
-run({
-  part1: {
-    tests: [
-      {
-        input: `
+function getSeedLocation(seed, maps) {
+  let loc = seed
+  for (let map of maps) {
+    for (const line of map) {
+      const [dest, src, range] = line
+
+      if (src <= loc && loc < src + range) {
+        loc = dest + (loc - src)
+        break
+      }
+    }
+  }
+
+  return loc
+}
+
+const input = `
         seeds: 79 14 55 13
 
         seed-to-soil map:
@@ -98,7 +133,13 @@ run({
         humidity-to-location map:
         60 56 37
         56 93 4
-        `,
+        `
+
+run({
+  part1: {
+    tests: [
+      {
+        input,
         expected: 35,
       },
     ],
@@ -106,13 +147,13 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input,
+        expected: 46,
+      },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: false,
+  onlyTests: true,
 })
